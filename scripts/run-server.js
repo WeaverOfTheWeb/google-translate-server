8a30fd8b970fb51d5828a10d55605ca1a52762f3
Lewis <1872377+WeaverOfTheWeb@users.noreply.github.com>
2019-01-14 13:14:53 +0000

    Allow CORS only to single domain

--- scripts/run-server.js
+++ scripts/run-server.js
@@ -10,10 +10,10 @@ const app = express();
 function startServer() {
 	var port = process.env.PORT;
 	
-	// CORS test
+	// CORS
 	var allowCrossDomain = function(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header('Access-Control-Allow-Origin', 'mememachine.pw');
+		res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
 		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
 		// intercept OPTIONS method
