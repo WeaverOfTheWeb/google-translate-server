ac12e1cc11b7ea06f834cdf008acef02f4577da6
Lewis <1872377+WeaverOfTheWeb@users.noreply.github.com>
2019-01-15 16:20:09 +0000

    Update run-server.js

--- scripts/run-server.js
+++ scripts/run-server.js
@@ -12,9 +12,10 @@ function startServer() {
 	
 	// CORS
 	var allowCrossDomain = function(req, res, next) {
-		res.header('Access-Control-Allow-Origin', '*');
+		//res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Origin', 'https://mememachine.pw');
 		res.header('Access-Control-Allow-Methods', 'GET,POST');
-		res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
 
 		// intercept OPTIONS method
 		if ('OPTIONS' == req.method) {
