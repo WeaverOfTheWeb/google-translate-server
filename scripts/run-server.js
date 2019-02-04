19ff3ce994cc9e6fe23082b86329738342bc9d15
Lewis <1872377+WeaverOfTheWeb@users.noreply.github.com>
2019-02-04 13:33:01 +0000

    Update run-server.js

--- scripts/run-server.js
+++ scripts/run-server.js
@@ -12,8 +12,7 @@ function startServer() {
 	
 	// CORS
 	var allowCrossDomain = function(req, res, next) {
-		//res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Origin', 'https://mememachine.pw');
+		res.header('Access-Control-Allow-Origin', 'https://avatheai.com');
 		res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTION');
 		res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
 
